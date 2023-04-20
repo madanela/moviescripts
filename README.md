5b7479dfed19d74b857706ee811a2ce4122f00b4
Madanela <alen.am0161@gmail.com>
2023-04-21 00:34:29 +0400

    bg fix

--- README.md
+++ README.md
@@ -3,9 +3,23 @@ The following is classification over movie scripts
 
 To run code, download each data from its kaggle link (see data/raw/*/README.md) and paste under their specific path data/raw/*/?
 
-# Data Preprocessing
+## Data Preprocessing
 
 You have to use 
-```
+```yaml
 dvc repro starwars
+```
+## Installation
+
+Check `scripts/init.bash` for more details.
+
+## Training
+
+Train moviescripts Bert model on the star wars dataset
+```yaml
+poetry run train
+```
+## Testing
+```yaml
+poetry run test
 ```
--- moviescripts/models/Classifers.py
+++ moviescripts/models/Classifers.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from transformers import BertForPreTraining,BertForSequenceClassification
+from transformers import BertForPreTraining,BertForSequenceClassification,SentenceTransformer
 
 
 
@@ -22,6 +22,37 @@ class BertSentenceClassifier(nn.Module):
     def forward(self, input_ids, attention_mask):
         # bert_1 = self.bert_freezed(input_ids=input_ids, attention_mask=attention_mask)
         bert_2 = self.bert_training(input_ids=input_ids, attention_mask=attention_mask)
+
+        x = nn.functional.relu(self.lin1(bert_2.pooler_output))
+
+        x = nn.functional.dropout(x, self.dropout_rate)
+
+        for lin_layer in self.lin_layers:
+
+            x = nn.functional.relu(lin_layer(x))
+            x = nn.functional.dropout(x, self.dropout_rate)
+
+        x = self.lin2(x)
+        return x
+
+class BertSentenceClassifier_MeanSentence(nn.Module):
+    def __init__(self, num_labels):
+        super(BertSentenceClassifier, self).__init__()
+
+        # self.bert_freezed =  BertForPreTraining.from_pretrained('distilbert-base-uncased') #BertForSequenceClassification.from_pretrained('bert-base-uncased', problem_type="multi_label_classification")
+        self.bert_training = SentenceTransformer('bert-base-nli-mean-tokens')
+        #BertForPreTraining.from_pretrained('distilbert-base-uncased') #BertForSequenceClassification.from_pretrained('bert-base-uncased', problem_type="multi_label_classification")
+        #BertForSequenceClassification.from_pretrained("bert-base-uncased", problem_type="multi_label_classification")
+        for param in self.bert_training.parameters():
+            param.requires_grad = False
+        self.dropout_rate = 0.1
+        self.lin1 = nn.Linear(768, 256)
+        self.lin_layers = nn.ModuleList([nn.Linear(256, 256) for i in range(1)])
+        self.lin2 = nn.Linear(256, num_labels)
+
+    def forward(self, text):
+        # bert_1 = self.bert_freezed(input_ids=input_ids, attention_mask=attention_mask)
+        bert_2 = self.bert_training.encode(text)
 
         x = nn.functional.relu(self.lin1(bert_2.pooler_output))
 
